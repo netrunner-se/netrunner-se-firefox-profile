e254fe8fe49f6ff707f68f5c59fd5c10f1b17e2b
Leszek Lesner <leszek.lsner@web.de>
2013-07-22 12:28:00 +0200

    * Initial commit

---
+++ etc/skel/.mozilla/firefox/se-default/extensions/ich@maltegoetz.de/defaults/preferences/prefs.js
@@ -0,0 +1,9 @@
+pref("extensions.proxtube.intpref_status", 1);
+pref("extensions.proxtube.intpref_firststart", 1);
+pref("extensions.proxtube.boolpref_agreementaccepted", false);
+pref("extensions.proxtube.boolpref_shownotf", false);
+pref("extensions.proxtube.boolpref_showsslnotf", false);
+pref("extensions.proxtube.intpref_enabled", 0);
+pref("extensions.proxtube.intpref_http", 0);
+pref("extensions.proxtube.stringpref_http", "");
+pref("extensions.proxtube.boolpref_showfoxydeal", false);
